62418b16e387e34ad53d26239fa71fc20b1f0eda
Marcel Strzałka <marcel.strzalka@monterail.com>
2022-09-14 11:11:17 +0200

    Add 2 helper methods for roles.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def is_customer?
+    current_user && current_user.customer?
+  end
+
+  def is_manager?
+    current_user && current_user.manager?
+  end
 end
