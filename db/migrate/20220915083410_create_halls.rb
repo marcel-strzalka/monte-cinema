f8469af5fbc982118a34660133363cedd9b9a654
Marcel Strzałka <marcel.strzalka@monterail.com>
2022-09-16 11:21:34 +0200

    Remove migration that renames Cinema Hall to Hall.

--- db/migrate/20220915083410_create_halls.rb
+++ db/migrate/20220915083410_create_halls.rb
@@ -1,6 +1,6 @@
-class CreateCinemaHalls < ActiveRecord::Migration[7.0]
+class CreateHalls < ActiveRecord::Migration[7.0]
   def change
-    create_table :cinema_halls do |t|
+    create_table :halls do |t|
       t.string :name
       t.integer :capacity
 
--- db/migrate/20220916082459_rename_cinema_halls_to_halls.rb
+++
@@ -1,5 +0,0 @@
-class RenameCinemaHallsToHalls < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :cinema_halls, :halls
-  end
-end
