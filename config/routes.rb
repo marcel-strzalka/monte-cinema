5434ce4ed1ed2355a6053a2fb614376cf8378011
Marcel Strzałka <marcel.strzalka@monterail.com>
2022-09-21 14:11:27 +0200

    Remove edit and update routes for Shows.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :halls
   resources :movies
-  resources :shows
+  resources :shows, except: [:edit, :update]
   root "movies#index"
 end
