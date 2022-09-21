4d1d5c54b8ebe81cf5a0215d4fbf1167ecee7182
Marcel Strzałka <marcel.strzalka@monterail.com>
2022-09-21 14:13:11 +0200

    Update Hall and Movie models.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Movie < ApplicationRecord
+  has_many :shows, dependent: :destroy
+
   validates :title, length: { minimum: 1 }
   validates :length_in_minutes, numericality: { in: 1..300 }
+
+  scope :sorted_alphabetically, -> { order(:title) }
 end
