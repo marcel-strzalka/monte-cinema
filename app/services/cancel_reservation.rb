80436ffa8550a845504f18ebd8e98dd84c32f6ac
Marcel Strzałka <marcel.strzalka@monterail.com>
2022-09-27 13:22:31 +0200

    Add CancelReservation service.

---
+++ app/services/cancel_reservation.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class CancelReservation
+  def call
+    return unless reservation.booked?
+
+    reservation.status = :canceled
+    reservation.save
+  end
+
+  private
+
+  def initialize(reservation)
+    @reservation = reservation
+  end
+
+  attr_reader :reservation
+end
