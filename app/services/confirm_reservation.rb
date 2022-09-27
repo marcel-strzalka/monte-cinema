e1625a6c6d34f28570b6de08e68af351f48fe79e
Marcel Strzałka <marcel.strzalka@monterail.com>
2022-09-27 13:22:49 +0200

    Add ConfirmReservation service.

---
+++ app/services/confirm_reservation.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class ConfirmReservation
+  def call
+    return unless reservation.booked?
+
+    reservation.status = :confirmed
+    reservation.save
+  end
+
+  private
+
+  def initialize(reservation)
+    @reservation = reservation
+  end
+
+  attr_reader :reservation
+end
