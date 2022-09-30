098acca568964a78b04261eed601977502ee2922
Marcel Strzałka <marcel.strzalka@monterail.com>
2022-09-30 14:00:53 +0200

    Configure App Controller for Pundit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_back fallback_location: root_path
+  end
+
   def customer?
     current_user&.customer?
   end
