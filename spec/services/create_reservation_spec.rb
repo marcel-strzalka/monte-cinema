1f40aa82403e4be24e3b37e7d03f15bd3967266d
Marcel Strzałka <marcel.strzalka@monterail.com>
2022-10-10 12:29:22 +0200

    Add CreateReservation spec.

---
+++ spec/services/create_reservation_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CreateReservation do
+  subject { described_class.new(reservation:, seat_numbers:).call }
+
+  let(:hall) { create :hall }
+  let(:movie) { create :movie }
+  let(:show) { create :show }
+  let(:user) { create :user }
+  let(:reservation) { Reservation.new(show:, user:) }
+
+  describe '#call' do
+    context 'when seat numbers are empty' do
+      let(:seat_numbers) { [] }
+
+      it 'returns false and adds an error' do
+        expect(subject).to be(false)
+        expect(reservation.errors[:base]).to include('You need to pick at least one seat')
+      end
+    end
+
+    context 'when seat number is invalid' do
+      shared_examples 'invalid seat number' do |seat_number|
+        let(:seat_numbers) { [seat_number] }
+
+        it 'returns false and adds an error' do
+          expect(subject).to be(false)
+          expect(reservation.errors[:base]).to include('One of provided seats is invalid')
+        end
+      end
+
+      context 'when seat number is less than 1' do
+        include_examples 'invalid seat number', '0'
+      end
+
+      context 'when seat number is greater than hall capacity' do
+        include_examples 'invalid seat number', '3'
+      end
+
+      context 'when seat number is not a number' do
+        include_examples 'invalid seat number', 'A'
+      end
+    end
+
+    context 'when seat number is taken' do
+      let(:seat_numbers) { ['1'] }
+
+      it 'returns false and adds an error' do
+        CreateReservation.new(reservation: Reservation.new(show:, user:), seat_numbers:).call
+        expect(subject).to be(false)
+        expect(reservation.errors[:base]).to include('One of provided seats is already taken')
+      end
+    end
+
+    context 'when every seat is taken' do
+      let(:seat_numbers) { %w[1 2] }
+
+      it 'returns false and adds an error' do
+        CreateReservation.new(reservation: Reservation.new(show:, user:), seat_numbers:).call
+        expect(subject).to be(false)
+        expect(reservation.errors[:base]).to include('Sorry, no seats are available for this show')
+      end
+    end
+
+    context 'when seat numbers are valid and available' do
+      let(:seat_numbers) { %w[1 2] }
+
+      it 'returns true' do
+        expect(subject).to be(true)
+      end
+
+      it 'creates a reservation' do
+        expect { subject }.to change { Reservation.count }.by(1)
+      end
+
+      it 'creates 2 tickets' do
+        expect { subject }.to change { Ticket.count }.by(2)
+      end
+    end
+  end
+end
