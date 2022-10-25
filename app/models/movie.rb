5a6f6d93495b5479345e203fa24a0d137264a5c8
Marcel Strzałka <marcel.strzalka@monterail.com>
2022-10-25 14:30:58 +0200

    Remove old Movie validations.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,8 +3,5 @@
 class Movie < ApplicationRecord
   has_many :shows, dependent: :destroy
 
-  validates :title, length: { minimum: 1 }
-  validates :length_in_minutes, numericality: { in: 1..300 }
-
   scope :sorted_alphabetically, -> { order(:title) }
 end
