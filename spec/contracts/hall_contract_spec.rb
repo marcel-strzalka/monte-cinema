73687aacb79ba7d7dbd500580b2b21fe9affd58b
Marcel Strzałka <marcel.strzalka@monterail.com>
2022-10-25 12:53:36 +0200

    Improve HallContract spec.

--- spec/contracts/hall_contract_spec.rb
+++ spec/contracts/hall_contract_spec.rb
@@ -4,24 +4,24 @@ require 'rails_helper'
 
 RSpec.describe HallContract do
   let(:hall_params) { { name: 'H', capacity: 1 } }
-  subject { described_class.new(hall: Hall.new).call(hall_params) }
+  subject(:result) { described_class.new(hall: Hall.new).call(hall_params) }
 
   describe '#call' do
     context 'when name is already taken' do
-      let!(:hall) { create :hall, name: 'H' }
+      before { create(:hall, name: 'H') }
 
       it 'returns failure' do
-        expect(subject.failure?).to be(true)
+        expect(result.failure?).to be(true)
       end
 
       it 'adds an error' do
-        expect(subject.errors.to_h).to eq({ name: ['is taken'] })
+        expect(result.errors.to_h).to eq({ name: ['is taken'] })
       end
     end
 
     context 'when hall params are valid' do
       it 'returns success' do
-        expect(subject.success?).to be(true)
+        expect(result.success?).to be(true)
       end
     end
   end
